58578b81b4026172b85f86b1114da8f64925561a
bybeet <travis.bybee@gmail.com>
2012-10-28 16:00:06 -0600

    Added button functionality. Title for problems changes based on button pushed. Added onClick listeners for buttons in the onCreate() of the main menu screen. Added a ProblemType enum that is set based on the button pushed. Add generateProblem that will create a problem based on the problem type set.

--- AndTutor/src/com/csci422/tutor/AdditionActivity.java
+++ AndTutor/src/com/csci422/tutor/AdditionActivity.java
@@ -2,14 +2,46 @@ package com.csci422.tutor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class AdditionActivity extends Activity {
 
+	private enum ProblemType {ADDITION, SUBTRACTION, BOTH};
+	
+	TextView title;
+	ProblemType problemType;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addition);
+        
+        Intent intent = getIntent();
+        title = (TextView)findViewById(R.id.title);
+        String newTitle = intent.getStringExtra("title");
+        
+        if(newTitle.equals("add")){
+        	title.setText(R.string.addition);
+        	problemType = ProblemType.ADDITION;
+        }
+        else if(newTitle.equals("sub")) {
+        	title.setText(R.string.subtraction);
+        	problemType = ProblemType.SUBTRACTION;
+        }
+        else if(newTitle.equals("both")) {
+        	title.setText(R.string.both);
+        	problemType = ProblemType.BOTH;
+        }
+        
+        generateProblem();
+    }
+    
+    private void generateProblem() {
+    	
     }
 
     @Override
--- AndTutor/src/com/csci422/tutor/MenuScreen.java
+++ AndTutor/src/com/csci422/tutor/MenuScreen.java
@@ -2,14 +2,51 @@ package com.csci422.tutor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuScreen extends Activity {
 
+	Button addition;
+	Button subtraction;
+	Button both;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+        
+        addition = (Button)findViewById(R.id.addition);
+        addition.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuScreen.this, com.csci422.tutor.AdditionActivity.class);
+				intent.putExtra("title", "add");
+				startActivity(intent);
+			}
+		});
+        
+        subtraction = (Button)findViewById(R.id.subtraction);
+        subtraction.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuScreen.this, com.csci422.tutor.AdditionActivity.class);
+				intent.putExtra("title", "sub");
+				startActivity(intent);
+			}
+		});
+        
+        both = (Button)findViewById(R.id.both);
+        both.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuScreen.this, com.csci422.tutor.AdditionActivity.class);
+				intent.putExtra("title", "both");
+				startActivity(intent);
+			}
+		});
     }
 
     @Override
